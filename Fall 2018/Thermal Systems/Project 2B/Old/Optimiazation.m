6b190422dafa086fabb2851989191407ab07b538
Charlie James Nitschelm <39833752+cjn1012@users.noreply.github.com>
2018-12-01 16:38:01 -0500

    thermal

---
+++ Fall 2018/Thermal Systems/Project 2B/Old/Optimiazation.m
@@ -0,0 +1,117 @@
+function efficiency = Optimization[Eff_Comp,Eff_Turbine,Eff_Nozzle]
+
+
+% Thermal Systems - Project 2A   
+% Function to obtain performance and efficient of a jet engine with particular parameters for non constant cp
+
+T1 = 300;
+P1 = 100;
+PR = 20;
+MaxT = 1500;
+
+
+%%%%%%
+% Definition of Constants
+%%%%%%
+Air_Data = xlsread('air_data1.xls');
+Temperatures    = Air_Data(:,1);
+Specific_Enthalpy = Air_Data(:,3);
+Specific_Entropy = Air_Data(:,4);
+
+R=0.287;     % Constant
+
+%%%%%%
+% State Calculations
+%%%%%%
+
+% State 1 % Inlet of Compressor 
+v1 = (R*T1)/P1; 
+s1 = interp1(Temperatures,Specific_Entropy,T1);
+h1 = interp1(Specific_Entropy,Specific_Enthalpy,s1);
+st1 = s1;
+
+% State 2 % Inlet of Combuster  
+P2 = PR*P1; 
+s2 = s1;
+st2 = st1 + R*log(PR);
+T2 = interp1(Specific_Entropy,Temperatures,st2);
+h2 = interp1(Temperatures,Specific_Enthalpy,T2);
+v2 = R*T2/P2;
+
+% State 3 % Inlet of Turbine
+P3 = P2; % Isobaric Process
+s3 = interp1(Temperatures,Specific_Entropy,MaxT)-st2+s2;
+st3 = interp1(Temperatures,Specific_Entropy,MaxT);
+v3 = (R * MaxT)/P3; 
+h3 = interp1(Temperatures,Specific_Enthalpy,MaxT);
+
+% State 4 % Inlet of Nozzle
+s4 = s3;
+T4 = interp1(Specific_Entropy,Temperatures,s4);
+st4 = interp1(Temperatures,Specific_Entropy,T4);
+h4 = h3 - (h2-h1);
+P4 = P3*(exp((st4-st3)/R));
+v4 = R*T4/P4;
+
+% State 5 % Outlet of Nozzle
+s5 = s4;
+P5 = P1; 
+st5 = st4 + R*log(P5/P4);
+T5 = interp1(Specific_Entropy,Temperatures,st5) ;
+v5 = (R*T5)/P5; 
+h5 = interp1(Temperatures,Specific_Enthalpy,T5);
+
+
+%%%%%%
+% State Calculations Non-Ideal
+%%%%%%
+
+Eff_Comp = .95;
+Eff_Turb = .9;
+Eff_Nozz = .9;
+
+
+% State 2 % Inlet of Combuster  
+P2n = PR*P1; 
+h2n = (h2-h1)/Eff_Comp + h1;
+T2n = interp1(Specific_Enthalpy,Temperatures,h2n);
+st2n = st1 + R*log(PR);
+s2n  = interp1(Specific_Enthalpy,Specific_Entropy,h2n)-st2+s1;
+
+% State 3 % Inlet of Turbine
+P3n = P2-300; % Isobaric Process
+s3n = interp1(Temperatures,Specific_Entropy,MaxT)-st2n+s2n;
+st3n = interp1(Temperatures,Specific_Entropy,MaxT);
+h3n = interp1(Temperatures,Specific_Enthalpy,MaxT);
+
+% State 4 % Inlet of Nozzle
+h4n = -Eff_Turb * (h3n-h4) + h3n ; 
+st4n = interp1(Specific_Enthalpy,Specific_Entropy,h4n);
+s4n = interp1(Specific_Enthalpy,Specific_Entropy,h4n)-st3n+s3n;
+
+T4n = interp1(Specific_Enthalpy,Temperatures,h4n);
+P4n = P4;
+
+
+% State 5 % Outlet of Nozzle
+h5n = -Eff_Nozz * (h4n-h5) + h4n ; 
+s5n = interp1(Specific_Enthalpy,Specific_Entropy,h5n);
+st5n = interp1(Specific_Enthalpy,Specific_Entropy,h5n);
+T5n = interp1(Specific_Enthalpy,Temperatures,h5n);
+P5n = P1;
+
+
+Q_Add    = h3n - h2n;
+Q_Remove = h5n - h1;
+
+W_Compressor = h2n - h1;
+W_Turbine    = h4n - h3n;
+
+Performance = W_Compressor + Q_Add - Q_Remove + W_Turbine;
+Thermal_Efficiency = Performance/Q_Add;
+
+
+
+
+
+
--- Fall 2018/Thermal Systems/Project 2B/Part_1.m
+++ Fall 2018/Thermal Systems/Project 2B/Part_1.m
@@ -7,154 +7,56 @@
     
 % Choose the best percentages and graph a T-s diagram
 
-%%%%%%
-% Definition of Constants
-%%%%%%
+clear all
+close all
 
-Air_Data = xlsread('air_data1.xls');
-Temperatures    = Air_Data(:,1);
-Specific_Enthalpy = Air_Data(:,3);
-Specific_Entropy = Air_Data(:,4);
+CompressorEff = linspace(.70,.90,21);
+TurbineEff = linspace(.78,.98,21);
+NozzleEff = linspace(.70,.90,21);
 
-Efficiency_Compressor = .9;
-Efficiency_Turbine    = .95;
-Efficiency_Nozzle     = .90;
+EffMatrix = zeros(200,4);
+l = 1;
 
-R = 0.287;     % Constant
-PR = 10;
-MaxT =1500;
-
-% Ideal Case
-
-% State 1 % Inlet of Compressor 
-T1a = 300;
-P1a = 100;
-s1a = interp1(Temperatures,Specific_Entropy,T1a);
-h1a = interp1(Specific_Entropy,Specific_Enthalpy,s1a);
-st1a = s1a;
-
-% State 2 % Inlet of Combuster  
-P2a = PR*P1a; 
-s2a = s1a;
-st2a = st1a + R*log(PR);
-T2a = interp1(Specific_Entropy,Temperatures,s2a);
-h2a = interp1(Temperatures,Specific_Enthalpy,T2a);
-
-% State 3 % Inlet of Turbine
-P3a = P2a; % Isobaric Process
-s3a = interp1(Temperatures,Specific_Entropy,MaxT)-st2a+s2a;
-st3a = interp1(Temperatures,Specific_Entropy,MaxT);
-h3a = interp1(Temperatures,Specific_Enthalpy,MaxT);
-
-% State 4 % Inlet of Nozzle
-s4a = s3;
-T4a = interp1(Specific_Entropy,Temperatures,s4a);
-st4a = interp1(Temperatures,Specific_Entropy,T4a);
-h4a = h3a - (h2a-h1a);
-P4a = P3a*(exp((st4a-st3a)/R));
-
-% State 5 % Outlet of Nozzle
-s5a = s4a;
-P5a = P1a; 
-st5a = st4a + R*log(P5a/P4a);
-T5a = interp1(Specific_Entropy,Temperatures,st5a) ;
-h5a = interp1(Temperatures,Specific_Enthalpy,T5a);
-
-%%%%%%
-% State Calculations for Actual Cycle
-%%%%%%
-
-% State 1 % Inlet of Compressor 
-s1 = s1a;
-h1 = h1a;
-st1 = st1a;
-
-% State 2 % Inlet of Combuster  
-P2 = P2a;
-st2 = st1 + R*log(PR);
-h2 = ((h2a-h1)/Efficiency_Compressor)+h1;
-T2 = interp1(Specific_Enthalpy,Temperatures,h2);
-s2 = interp1(Specific_Enthalpy,Specific_Entropy,h2)-st1+s1;
-
-% State 3 % Inlet of Turbine
-P3 = P2; % Isobaric Process
-s3 = interp1(Temperatures,Specific_Entropy,MaxT)-st2+s2;
-st3 = interp1(Temperatures,Specific_Entropy,MaxT);
-h3 = interp1(Temperatures,Specific_Enthalpy,MaxT);
-
-% State 4 % Inlet of Nozzle
-h4 = h3 - Efficiency_Turbine*(h3-h4a);
-T4 = interp1(Specific_Enthalpy,Temperatures,h4);
-s4 = interp1(Temperatures,Specific_Entropy,T4)-st3+s3;
-st4 = interp1(Temperatures,Specific_Entropy,T4);
-
-% State 5 % Outlet of Nozzle
-P5 = P1; 
-h5 = h4 - Efficiency_Nozzle*(h4-h5a);
-s5 = interp1(Specific_Enthalpy,Specific_Entropy,h5);
-T5 = interp1(Specific_Enthalpy,Temperatures,h5);
-
-
-%%%%%
-% Calculating the variables needed to find the 
-% overall efficiency and performance of the jet engine
-%%%%%
-
-Q_Add    = h3 - h2;
-Q_Remove = h5 - h1;
-
-W_Compressor = h2 - h1;
-W_Turbine    = h4 - h3;
-
-Performance = W_Compressor + Q_Add - Q_Remove + W_Turbine;
-Thermal_Efficiency = Performance/Q_Add;
-
-
-% Compressor Process - Isentropic - P as a function of v
-
-
-s_Compressor = linspace(s1,s2,1000)';
-T_Compressor = linspace(T1,T2,1000)';
-
-
-% Combustor Process - Isobaric - T as a function of s
-
-T_Combustor = linspace(T2,MaxT,1000)';
-s_Combustor = zeros(1000,1);
-
-for index = 1:1000
-    s_Combustor(index) = (interp1(Temperatures,Specific_Entropy,T_Combustor(index)))-st2+s2;
+for i = CompressorEff
+    for j = TurbineEff
+        for k = NozzleEff
+            if i+j+k == 2.60
+                EffMatrix(l,1) = i;
+                EffMatrix(l,2) = j;
+                EffMatrix(l,3) = k;
+                EffMatrix(l,4) = Optimization(i,j,k);
+                l = l + 1;
+            end
+        end
+    end
 end
 
-% Turbine Process - Isentropic - P as a function of v
+                
+Sorted = sort(EffMatrix,4);                
 
-s_Turbine = linspace(s3,s4,1000)';
-T_Turbine = linspace(MaxT,T4,1000)';
+Eff_Comp = Sorted(1,1);
+Eff_Turb = Sorted(1,2);
+Eff_Nozz = Sorted(1,3);
+
+[EntropyIdeal,TemperatureIdeal,EntropyActual,TemperatureActual] = T_s_Data(Eff_Comp,Eff_Turb,Eff_Nozz);
+
+plot(EntropyIdeal,TemperatureIdeal,'b')
+hold on
+plot(EntropyActual,TemperatureActual,'r')
 
 
 
-% Nozzle Process - Isentropic - P as a function of v
 
-s_Nozzle = linspace(s4,s5,1000)';
-T_Nozzle = linspace(T4,T5,1000)';
 
-% Reset Process - 4 to 1 - Isobaric
 
-T_Reset = linspace(T5,T1,1000)';
-s_Reset = zeros(1000,1);
 
-for index = 1:1000
-    s_Reset(index) = interp1(Temperatures,Specific_Entropy,T_Reset(index))-st1+s1;
-end
 
-% Data Compiled for Output - 1000 points per process
-Temperature_Cycle     = [T_Compressor;T_Combustor;T_Turbine;T_Nozzle;T_Reset];
-SpecificEntropy_Cycle = [s_Compressor;s_Combustor;s_Turbine;s_Nozzle;s_Reset];
 
-figure(1)
-plot(s_Compressor,T_Compressor)
-figure(2)
-plot(SpecificEntropy_Cycle,Temperature_Cycle)
+
+
+
+
+
 
 
 
