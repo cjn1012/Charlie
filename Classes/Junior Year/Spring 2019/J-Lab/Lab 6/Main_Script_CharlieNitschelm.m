a631dc0a510daa6f09da6e59d0324286e077c35e
Charlie James Nitschelm <39833752+cjn1012@users.noreply.github.com>
2019-04-10 11:02:54 -0400

    Create Main_Script_CharlieNitschelm.m

---
+++ Classes/Junior Year/Spring 2019/J-Lab/Lab 6/Main_Script_CharlieNitschelm.m
@@ -0,0 +1,51 @@
+clear all 
+close all
+
+% Part 1
+Temp_I = 21.2+273.15; % Deg C
+Temp_F = 22.6+273.15; % Deg C
+Pres_I = 102268; % Pa
+Pres_F = 102235; % Pa
+R = 8.314; % J/molK
+MW = 28.97/1000; % g/mol
+
+Density_I = (MW*Pres_I)/(R*Temp_I); % kg/m3
+Density_F = (MW*Pres_F)/(R*Temp_F); % kg/m3
+
+% Part 2
+AoA_Foil_0  = [1.6,3.2,2.95,3.54,3.85,3.57,3.79,3.65,3.52,3.05,3.31,3.16,2.98,2.55,3.65,3.44,3.21,3.5,3.4,3.31,2.91,3.14,.49].*0.0254;
+AoA_Foil_9  = [1.6,3.2,7,5.9,5.25,4.4,4.2,3.8,3.55,3.05,3.25,3.15,1.6,2.2,2.6,2.65,2.7,3.05,3.1,3.05,2.7,2.9,.45].*0.0254;
+AoA_Foil_18 = [1.6,3.25,4.05,3.95,4.00,3.7,4.1,4.15,4.2,3.65,4.0,3.85,1.5,1.9,2.25,2.35,2.5,2.9,3.0,3.1,2.85,3.25,0.5].*0.0254;
+
+Den_Water = 998; %kg/m3
+Gravity = 9.81; %m/s2
+
+Pres_Foil_0 =  Pres_I - Den_Water*Gravity.*AoA_Foil_0;
+Pres_Foil_9 =  Pres_I - Den_Water*Gravity.*AoA_Foil_9;
+Pres_Foil_18 =  Pres_I - Den_Water*Gravity.*AoA_Foil_18;
+
+% Part 3
+Vel_Foil_0 = sqrt((2/Density_I)*(Pres_Foil_0(1)-Pres_Foil_0(2)));
+Vel_Foil_9 = sqrt((2/Density_I)*(Pres_Foil_9(1)-Pres_Foil_9(2)));
+Vel_Foil_18 = sqrt((2/Density_I)*(Pres_Foil_18(1)-Pres_Foil_18(2)));
+Vel_Foil_Mean = mean([Vel_Foil_0,Vel_Foil_9,Vel_Foil_18]);
+
+% Part 4
+
+L_Force = [-9.3,-6.3,-3,0.4,3.3,6.3,9,11.5,13.5,11.1,10.8];
+D_Force = [1.5,1.1,0.8,0.75,0.85,1.1,1.5,1.95,2.5,5.1,5.7];
+A = .15*.30; % Chord * Span
+C_L = L_Force/(.5*Density_I*(Vel_Foil_Mean^2)*A);
+C_D = D_Force/(.5*Density_I*(Vel_Foil_Mean^2)*A);
+
+
+
+
+
+
+
+
+
+
+
+
