4b590b6ce145a2f095741450e0295daa86c01cda
Charlie James Nitschelm <39833752+cjn1012@users.noreply.github.com>
2019-05-01 10:51:30 -0400

    yeetr

--- Classes/Junior Year/Spring 2019/J-Lab/Lab 7/MainCode_CN.m
+++ Classes/Junior Year/Spring 2019/J-Lab/Lab 7/MainCode_CN.m
@@ -8,12 +8,18 @@ Moment = MomentVoltage.*33405-.7733;
 D1 = 16; % Length of pin to load cell
 D2 = 43; % Length of pin to concentrated drag force
 
-Force = (Moment*D1)/D2
-Time = linspace(0,length(Force)/100,length(Force))
+Force = (Moment*D1)/D2 ;
+Force_Cal = 0 - Force(1);
+Force = Force + Force_Cal;
+Force = Force/.225;
+Time = linspace(0,length(Force)/100,length(Force))-2;
 
 figure(1)
 plot(Time,Force)
-
+xlabel('Time (s)')
+ylabel('Drag Force (N)')
+title('Drag Force - No Fairings')
+xlim([0,14])
 
 
 
@@ -26,11 +32,35 @@ D1 = 16; % Length of pin to load cell
 D2 = 43; % Length of pin to concentrated drag force
 
 Force2 = (Moment2*D1)/D2;
-Time2 = linspace(0,length(Force2)/100,length(Force2));
+Force2_Cal = 0 - Force2(3);
+Force2 = Force2 + Force2_Cal;
+Force2 = Force2/.225
+Time2 = linspace(0,length(Force2)/100,length(Force2))-5;
 
 figure(2)
 plot(Time2,Force2)
+xlabel('Time (s)')
+ylabel('Drag Force (N)')
+title('Drag Force - With Fairings')
+xlim([0,14])
 
+
+
+
+figure(3)
+subplot(2,1,1)
+plot(Time,Force)
+xlabel('Time (s)')
+ylabel('Drag Force (N)')
+title('Drag Force - No Fairings')
+xlim([0,14])
+
+subplot(2,1,2)
+plot(Time2,Force2)
+xlabel('Time (s)')
+ylabel('Drag Force (N)')
+title('Drag Force - With Fairings')
+xlim([0,14])
 
 
 
