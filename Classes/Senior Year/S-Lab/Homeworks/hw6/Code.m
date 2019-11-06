137834e3eb2f3d9b07cb803a5298edb3dc30a0f2
Charlie James Nitschelm <39833752+cjn1012@users.noreply.github.com>
2019-11-06 17:18:12 -0500

    for slab

--- Classes/Senior Year/S-Lab/Homeworks/hw6/Code.m
+++ Classes/Senior Year/S-Lab/Homeworks/hw6/Code.m
@@ -1,27 +1,28 @@
 clear all
 close all
 
-%Constants
-Ktach = .003
-R = 4.2
-J = .0004
-B = .002
-Ka = -1
-Kt = 6.602
-Ke = .047
-Ri = 100000
-Rf = 150000
-C1 = .000000033
-C2 = .00000001
 
 % Part b
 
-% p-control
 
-num1 = [Rf*Kt*Ktach/(B*R)]
-den1 = [Ri*J/B,Ri]
+% P-Control
+
+num1 = [168.4]
+den1 = [1,189.6]
+figure(1)
 sys = tf(num1,den1)
-
-
-
 rlocus(sys)
+
+% I-Control
+num2 = [34048]
+den2 = [1,189.6,0]
+figure(2)
+sys2 = tf(num2,den2)
+rlocus(sys2)
+
+% PI-Control
+num3 = [168.8,666.7*168.7]
+den3 = [1,189.6,0]
+figure(3)
+sys3 = tf(num3,den3)
+rlocus(sys3)
